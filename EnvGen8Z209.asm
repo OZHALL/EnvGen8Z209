1b0081a4be4613a6da8b948d14d8f56f0efb48fa
O.Z.Hall <o.z.hall@att.net>
2018-05-07 18:50:26 -0500

    First Modifications
    Add SPI based DAC output
    Change pins to Z209 hardware

--- EnvGen8Z209.asm
+++ EnvGen8Z209.asm
@@ -1,4 +1,92 @@
- title  "Voltage-Controlled VCADSR"
+; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
+; ozh - note I followed the configurations from another project which was MCC generated
+;2018-05-03 ozh - updated port_init with MCC generated pin_manager.  It compiles, but ...
+;		looks like I broke the funcdtionality.  Need to debug this new code.
+;2018-05-03 ozh - ANSELC configuration issue.  LEDs are digital pins, not analog
+;		  code works to flash LED
+;2018-05-06 ozh - once I configured SPI Mode 0, the DAC output works with the following:
+;	RB7 - SPI Data Out (MOSI)
+;       RB6 - SPI Clock
+;       RB5 - SPI /CS (for MCP4922)
+;       RC2 - SPI Data Out (MISO) - have to allocate the pin, though we don't use it
+;       RA0 thru RA7(AN0-AN7) are all faders - analog inputs
+;       RB0 thru RB4 are outputs driving the LEDs on the faders (0-4)
+;       RC5 thru RC7 are outputs driving the LEDs on the faders (5-7)  
+;2018-05-06 ozh - debug outputting to both DAC0 and DAC1  
+;2018-05-07 ozh - hand optimize where I can
+;2018-05-07 ozh - add EnvGen8 code and integrate with my hardware config and DAC output
+    
+; PIC16F18855 Configuration Bit Settings
+
+; Assembly source line config statements
+	LIST R=DEC
+	#include "p16f18855.inc"
+	Errorlevel -302 
+
+; CONFIG1
+; __config 0x3FEC
+ __CONFIG _CONFIG1, _FEXTOSC_OFF & _RSTOSC_HFINT1 & _CLKOUTEN_OFF & _CSWEN_ON & _FCMEN_ON
+; CONFIG2
+; __config 0x3FFF
+ __CONFIG _CONFIG2, _MCLRE_ON & _PWRTE_OFF & _LPBOREN_OFF & _BOREN_ON & _BORV_LO & _ZCD_OFF & _PPS1WAY_ON & _STVREN_ON
+; CONFIG3
+; __config 0x3F9F
+ __CONFIG _CONFIG3, _WDTCPS_WDTCPS_31 & _WDTE_OFF & _WDTCWS_WDTCWS_7 & _WDTCCS_SC
+; CONFIG4
+; __config 0x1FFF
+ __CONFIG _CONFIG4, _WRT_OFF & _SCANE_available & _LVP_OFF
+; CONFIG5
+; __config 0x3FFF
+ __CONFIG _CONFIG5, _CP_OFF & _CPD_OFF
+
+;------------------------------
+;	Variables
+;------------------------------
+
+; CBLOCK 0x020
+;	LOOP_COUNTER_1
+;	LOOP_COUNTER_2
+;	LEDCOUNTER
+; ENDC
+ ; 0x70-0x7F  Common RAM - Special variables available in all banks
+; CBLOCK 0x070
+ 	; The 12 bit output level
+;	OUTPUT_HI
+;	OUTPUT_LO
+;	DACNUMBER
+; ENDC
+ 
+;-------------------------------------
+;	DEFINE STATEMENTS
+;-------------------------------------
+
+; Useful bit definitions for clarity	
+;#define ZERO		STATUS,Z	; Zero Flag
+;#define CARRY		STATUS,C	; Carry Flag
+;#define BORROW		STATUS,C	; Borrow is the same as Carry
+#define NOT_CS		PORTB,5         ; RB5 
+#define BIT0 		b'00000001'
+#define BIT1 		b'00000010'
+#define BIT2 		b'00000100'
+#define BIT3 		b'00001000'
+#define BIT4 		b'00010000'
+#define BIT5 		b'00100000'
+#define BIT6 		b'01000000'
+#define BIT7 		b'10000000'
+#define STEPSIZE	0x10
+#define DAC0		0x00
+#define DAC1		BIT7
+#define SAVETOW		0x00
+#define SAVETOF		0x01
+#define USEACCESSBANK	0x00 ;untested
+#define USEBSR		0x01 ;untested
+#define LEDLAST		0x07 ; last LED on pin 7 of port C
+
+;================================================================
+; begin orig ENVGEN8 code
+;================================================================
+ 
+  title  "DUAL ADSR"
 ;============================================================================
 ;	ELECTRIC DRUID VOLTAGE CONTROLLED ADSR VERSION 8
 ;============================================================================
@@ -33,6 +121,21 @@
 ; 13  RA0/AN0		: 0-5V Attack CV
 ; 14  Gnd
 ;
+;	1) Change pin assignments in the ENVGEN8 code to match my PIC16F18855 hardware:
+;
+;	Function    Existing        My New Hardware
+;
+;	Attack -     RA1/AN0    RA0/ANA0
+;
+;	Decay -     RA2/AN2    RA1/ANA1
+;
+;	Sustain -   RC0/AN4    RA2/ANA2
+;
+;	Release -   RC1/AN5    RA3/ANA3
+;
+;	Trigger -    RA0            RC0
+;
+;	Gate -         RA1            RC0 (merge trigger &amp; gate cuz DOTCOM only has Gate)  
 ; This version started as ENVGEN7B.ASM, the ultimate version of my 16F684
 ; envelope code, developed between 29th Aug 2006 and 14th May 2008
 ;
@@ -46,13 +149,13 @@
 ; Tidying up code.
 
 
-	LIST R=DEC
-	INCLUDE <p16f1764.inc>
-	Errorlevel -302
+;	LIST R=DEC
+;	INCLUDE <p16f1764.inc>
+;	Errorlevel -302
 
 ; 16F1764/5 Configuration
- __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _BOREN_OFF & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
- __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_ON & _STVREN_ON & _BORV_LO & _LPBOR_OFF & _LVP_OFF
+; __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _BOREN_OFF & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
+; __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_ON & _STVREN_ON & _BORV_LO & _LPBOR_OFF & _LVP_OFF
 
 ; Assembler has a problem with these: PPS1WAY_OFF & ZCD_OFF
 ; Weird, since they're in the include file
@@ -121,9 +224,10 @@
 	; The current output level when an Attack or Release starts
 	START_HI
 	START_LO
-	; The 10 bit output level
+	; The 12 bit output level
 	OUTPUT_HI
 	OUTPUT_LO
+	DACNUMBER
  ENDC
 
 ;-------------------------------------
@@ -144,10 +248,14 @@
 #define LOOPING		FLAGS, 1	; Looping (Makes env loop whilst GATE high)
 
 ; Input definitions
-#define TRIG_CHANGED	CHANGES, 4	; RC4 = TRIGGER input
-#define TRIGGER			STATES, 4
-#define GATE_CHANGED	CHANGES, 5	; RC5 = GATE input
-#define GATE			STATES, 5
+; #define TRIG_CHANGED	CHANGES, 4	; RC4 = TRIGGER input
+; #define TRIGGER			STATES, 4
+; #define GATE_CHANGED	CHANGES, 5	; RC = GATE input
+; #define GATE			STATES, 5
+#define TRIG_CHANGED	CHANGES, 0	; RC0 = TRIGGER input
+#define TRIGGER			STATES, 0
+#define GATE_CHANGED	CHANGES, 0	; RC0 = GATE input
+#define GATE			STATES, 0
 ; Note I use the debounced variables, not the input directly
  
 ; Output DAC assignments, mostly for readability (Bank 11)
@@ -187,6 +295,8 @@ InterruptEnter:
 ; This could debounce eight inputs, but I'm using only two:
 ; RC4 Trigger and RC5 Gate
 	
+; Z209 consolidate Gate & Trigger & use RC0 for channel 1
+	
 	; First, increment the debounce counters
 	movfw	DEBOUNCE_LO	
 	xorwf	DEBOUNCE_HI, f		; HI+ = HI XOR LO
@@ -220,7 +330,9 @@ TestTrigger:
 	btfss	TRIG_CHANGED
 	goto	TestGate
 	; TRIGGER has changed, but has it gone high?
-	btfss	TRIGGER
+; Z209 - we need to reverse this cuz my hardware inverts the gate input
+;	btfss	TRIGGER
+	btfsc	TRIGGER
 	goto	TestGate			; No, so skip
 	
 StartEnvelope:
@@ -244,7 +356,9 @@ TestGate:
 	btfss	GATE_CHANGED
 	goto	GenerateEnvelope
 	; GATE has changed, but has it gone low?
-	btfsc	GATE
+; Z209 - we need to reverse this cuz my hardware inverts the gate input
+;	btfsc	GATE
+	btfss	GATE
 	goto	GenerateEnvelope	; No, so skip
 
 EndEnvelope:
@@ -542,14 +656,63 @@ ReleaseScaling:
 ;---------------------------------------
 DACOutput:
 	; Load 10-bit DAC
-	movlb	D'11'				; All DACs are in Bank 11
-	movf	OUTPUT_LO, w		; Move output data to DAC1
-	movwf	DAC1REFL
-	movf	OUTPUT_HI, w
-	movwf	DAC1REFH	
-	movlw	B'00000001'
-	movwf	DACLD				; Load DAC1 alone
+;	movlb	D'11'				; All DACs are in Bank 11
+;	movf	OUTPUT_LO, w		; Move output data to DAC1
+;	movwf	DAC1REFL
+;	movf	OUTPUT_HI, w
+;	movwf	DAC1REFH	
+;	movlw	B'00000001'
+;	movwf	DACLD				; Load DAC1 alone
+
+;	output to MCP4922, not the internal DAC
+	movlw DAC0	; TODO: change this hardcoded DAC0 to a variable
+        ; pass in the DAC # (in bit 7) via 
+	iorlw 0x30	    ;bit 6=0 (n/a); bit 5=1(GAin x1); bit 4=1 (/SHDN)
+        movwf DACNUMBER     
+
+	; output the OUTPUT_HI and OUTPUT_LO to the DAC# (0 or 1) specified in W
+	movlb 0		; PORTB
+	bcf   NOT_CS	; Take ~CS Low
+	nop		; settling time
 	
+	movlb 3
+;    // Clear the Write Collision flag, to allow writing
+	bcf SSP2CON1,WCOL   ;    SSP2CON1bits.WCOL = 0;
+	; not sure (if/why) we need this
+	movf  SSP2BUF,w  ; Do a dummy read to clear flags
+	
+	; first send high byte plus commands/configuration
+	movf OUTPUT_HI,w
+	andlw 0x0F	; we will only want least significant4 bits
+	;for DAC0 or DAC1 - dac # (bit 7) 
+	;                 bit 15 A/B: DACA or DACB Selection bit
+	iorwf DACNUMBER,0 ; clr or set bit based on DAC.  0=save into W 
+	; this works, but let's do all of this at the beginning
+;	;               ; 0 = unbuffered - bit 14  VREF Input Buffer Control bit
+;	iorlw BIT5	; set gain of 1 - bit 13 Output Gain Selection bit
+;	iorlw BIT4	; 0x10 - bit 12 SHDN: Output Shutdown Control bit
+	
+	; now W has the commands plus data bits 12-9
+	movwf SSP2BUF	; load the buffer
+WriteByteHiWait:
+	btfss	SSP2STAT, BF		; Wait while it sends
+	goto	WriteByteHiWait	
+
+	; not sure (if/why) we need this
+	movf  SSP2BUF,w  ; Do a dummy read to clear flags
+	; second send the low byte
+	movf OUTPUT_LO,w
+	movwf SSP2BUF	; load the buffer
+WriteByteLoWait:
+	btfss	SSP2STAT, BF		; Wait while it sends
+	goto	WriteByteLoWait	
+	
+	; end of write
+	movlb 0		; PORTB
+	bsf   NOT_CS	; Take ~CS high
+	; don't need this here
+;	nop		; settling time
+;	return	
 ;----------------------------------------
 InterruptExit:
 	retfie
@@ -801,16 +964,21 @@ DoADConversion:
 Main:
 	movlb	D'1'				; Bank 1
 
-	; Set up the clock for 32MHz internal
-	movlw	B'11110000'			; 8MHz internal, x4 PLL
-	movwf	OSCCON
-
-	; Set up the IO Ports
-	movlw   b'111111'			; All inputs RA0:RA5
-	movwf   TRISA
-	movlw   b'111011'			; RC2 Output, all others inputs
-	movwf   TRISC
-
+	call Init_Osc
+	call Init_Ports
+	
+;	; Set up the clock for 32MHz internal
+;	movlw	B'11110000'			; 8MHz internal, x4 PLL
+;	movwf	OSCCON
+;
+;	; Set up the IO Ports
+;	movlw   b'111111'			; All inputs RA0:RA5
+;	movwf   TRISA
+;	movlw   b'111011'			; RC2 Output, all others inputs
+;	movwf   TRISC
+;
+; TODO: review this setup
+;
 	; Set up the interrupts
 	bsf		INTCON, GIE			; Enable interrupts
 	bsf		INTCON, PEIE		; Enable peripheral interrupts
@@ -826,47 +994,49 @@ Main:
 	movlw	0x1F				; Set up Timer2 period register (/32)
 	movwf	T2PR				; Interrupts at 1MHz/32 = 31.25KHz
 
+; see Init_Ports for this configuration for Z209
 ; Set up Analog-to-digital convertor and ADC inputs
-	movlb	D'3'				; Bank 3
-	movlw	B'00010111'			; 4 Analog inputs on RA0-RA2, RA4
-	movwf	ANSELA
-	movlw	B'00001011'			; 3 Analog inputs on RC0, RC1, RC3
-	movwf	ANSELC
-
+;	movlb	D'3'				; Bank 3
+;	movlw	B'00010111'			; 4 Analog inputs on RA0-RA2, RA4
+;	movwf	ANSELA
+;	movlw	B'00001011'			; 3 Analog inputs on RC0, RC1, RC3
+;	movwf	ANSELC
+; TODO: review this setup
 	movlb	D'1'				; Bank 1
 	movlw	B'00000001'			; AN0, ADC on
 	movwf	ADCON0	
 	movlw	B'01100000'			; Left-justified, Fosc/64, Vss to Vdd range.
 	movwf	ADCON1
-	
+; not using internal DAC!	
 ; Set up DAC1 (10-bit) for Envelope Output
-	movlb	D'11'				; All DACs are in Bank 11
-	movlw	B'11000100'			; Enabled, L-justified, No output, Vref+, Vss
-;	movlw	B'11000000'			; Enabled, L-justified, No output, Vdd, Vss
-	movwf	DAC1CON0			; (Note that we use Vref+ as Level CV)
-	clrf	DAC1REFH			; Zero the value
-	clrf	DAC1REFL
-	
-; Set up op-amp to buffer DAC output
-	movlb	D'10'				; Bank 10
-	movlw	B'00000000'			; -in = default (we set unity gain in a mo)
-	movwf	OPA1NCHS
-	movlw	B'00000010'			; +in = DAC1
-	movwf	OPA1PCHS
-	movlw	B'10010000'			; Enabled, Unity Gain, no override
-	movwf	OPA1CON
-	movlw	B'00000000'			; Override selection (default)
-	movwf	OPA1ORS
+;	movlb	D'11'				; All DACs are in Bank 11
+;	movlw	B'11000100'			; Enabled, L-justified, No output, Vref+, Vss
+;;	movlw	B'11000000'			; Enabled, L-justified, No output, Vdd, Vss
+;	movwf	DAC1CON0			; (Note that we use Vref+ as Level CV)
+;	clrf	DAC1REFH			; Zero the value
+;	clrf	DAC1REFL
+;	
+;; Set up op-amp to buffer DAC output
+;	movlb	D'10'				; Bank 10
+;	movlw	B'00000000'			; -in = default (we set unity gain in a mo)
+;	movwf	OPA1NCHS
+;	movlw	B'00000010'			; +in = DAC1
+;	movwf	OPA1PCHS
+;	movlw	B'10010000'			; Enabled, Unity Gain, no override
+;	movwf	OPA1CON
+;	movlw	B'00000000'			; Override selection (default)
+;	movwf	OPA1ORS
 
-; Set up weak pull-ups on USE_ADSR and USE_EXPO inputs
-	movlb	D'1'				; Bank 1
-	movlw	B'01111111'
-	movwf	OPTION_REG			; Weak pull-ups enabled
-	movlb	D'4'				; Bank 4
-	movlw	B'000000'			; No Pull-ups on Port C
-	movwf	WPUC
-	movlw	B'101000'			; Pull-ups on RA3 and RA5
-	movwf	WPUA
+; not using these inputs at this time.  see Init_Ports
+;; Set up weak pull-ups on USE_ADSR and USE_EXPO inputs
+;	movlb	D'1'				; Bank 1
+;	movlw	B'01111111'
+;	movwf	OPTION_REG			; Weak pull-ups enabled
+;	movlb	D'4'				; Bank 4
+;	movlw	B'000000'			; No Pull-ups on Port C
+;	movwf	WPUC
+;	movlw	B'101000'			; Pull-ups on RA3 and RA5
+;	movwf	WPUA
 
 	
 ; Set up initial values of the variables
@@ -882,6 +1052,16 @@ Main:
 	clrf	DECAY_CV
 	clrf	SUSTAIN_CV
 	clrf	RELEASE_CV
+; TODO: remove this once ADC is working
+;	set up a default percussive envelope for Z209
+	movlw	0x7f				
+	movwf	DECAY_CV
+	movlw	0x7f
+	movwf	SUSTAIN_CV
+	movlw	0x3f
+	movwf	RELEASE_CV
+; end percussive preset
+	
 	clrf	TIME_CV				; Default to no time modulation
 	clrf	MODE_CV				; Default to standard ADSR, no looping
 	clrf	STAGE
@@ -935,13 +1115,16 @@ MainLoop:
 SelectADCChannel:
 	movf	ADC_CHANNEL, w		; Get current channel
 	andlw	D'7'				; Only want 3 LSBs
-	brw							; Computed branch
-	goto	AttackCV
-	goto	DecayCV
-	goto	SustainCV
-	goto	ReleaseCV
-	goto	TimeCV
-	goto	ModeCV
+	; skip ADC until the DAC output is working with preset values - OZH
+	; TODO: reintroduce the ADC later
+;	brw							; Computed branch
+;	goto	AttackCV
+;	goto	DecayCV
+;	goto	SustainCV
+;	goto	ReleaseCV
+	; don't process these channels, leave them hardcoded
+;	goto	TimeCV
+;	goto	ModeCV
 
 ScannedAllChannels:
 	; Reset ADC channel 
@@ -1002,7 +1185,9 @@ DecayCV:
 
 ; Update the Sustain CV
 SustainCV:
-	movlw	b'00010001'			; AN4, ADC On
+;	movlw	b'00010001'			; AN4, ADC On
+    ; Question: in what way is the AN4? (looks like AN2 to me)
+	movlw	b'00010010'			; AN2, ADC On	Z209
 	call	DoADConversion
 	movwf	SUSTAIN_CV			; Simply store this one- easy!
 	goto	MainLoop
@@ -1010,7 +1195,8 @@ SustainCV:
 
 ; Update the Release CV
 ReleaseCV:
-	movlw	b'00010101'			; AN5, ADC On
+;	movlw	b'00010101'			; AN5, ADC On
+	movlw	b'00010011'			; AN3, ADC On	Z209
 	call	DoADConversion
 	movwf	RELEASE_CV
 	; Subtract the TIME_CV (Increasing TIME_CV shortens the Env)
@@ -1405,4 +1591,175 @@ DecayCurve:
 	dt	D'255', D'255'
 
 ; We never reach here
-	end 
+;	end 
+;================================================================
+; end orig ENVGEN8 code
+;================================================================ 
+
+    ; ***** Miscellaneous Routines*********************************************
+
+; -----------------------------------------------------------------------
+; The pins on the PIC are organized into ports.  Each port is about 8 bits wide.  However this
+; is only a general rule and your device may vary.
+
+; To setup a particular pin, we need to put values into the corresponding Analog Select
+; register, and Tri-state register.  This ensures that our pin will be an output, and that
+; it will be a digital output.
+	cblock
+	    GIE_STATE	;variable
+	endc
+Init_Ports
+; convert working C code from DualEG (mcc generated) to ASM
+;void PIN_MANAGER_Initialize(void)
+	movlb 0
+	clrf LATA   ;    LATA = 0x00;
+	movlw 0x20
+	movwf LATB  ;    LATB = 0x20;  
+	clrf LATC   ;    LATC = 0x00; 
+
+;	movlb 0
+	movlw 0xFF
+	movwf TRISA ;    TRISA = 0xFF;
+	clrf TRISB  ;    TRISB = 0x00;
+	movlw 0x1F
+	movwf TRISC ;    TRISC = 0x1F;
+
+;   analog/digital (GPIO)
+	movlb d'30'
+	movlw 0x04
+	movwf ANSELC	;    ANSELC = 0x04;
+	movlw 0x00
+	movwf ANSELB	;    ANSELB = 0x00;
+	movlw 0xFF
+	movwf ANSELA	;    ANSELA = 0xFF;
+	
+;   weak pullup
+;	movlb d'30'
+	clrf WPUE   ;    WPUE = 0x00;
+	movlw 0xE0
+	movwf WPUB  ;    WPUB = 0xE0;
+	clrf WPUA   ;    WPUA = 0x00;
+	clrf WPUC   ;    WPUC = 0x00;
+
+;   open drain
+;	movlb d'30'
+	clrf ODCONA ;    ODCONA = 0x00;
+	clrf ODCONB ;    ODCONB = 0x00;
+	clrf ODCONC ;    ODCONC = 0x00;   
+	
+;   preserve the GIE state - global interrupt enable
+;    bool state = (unsigned char)GIE;
+	movf  INTCON,w
+	andlw b'10000000'   ;isolate bit 7
+	movwf GIE_STATE
+;    GIE = 0;	shut it off for to do the config
+	movf  INTCON,w
+	andlw b'01111111'   ;clear bit 7
+	movwf INTCON
+	
+;   PPSLOCK takes a special sequence to unlock
+	movlb d'29'
+	movlw 0x55
+	movwf PPSLOCK ;    PPSLOCK = 0x55;
+	movlw 0xAA
+	movwf PPSLOCK ;    PPSLOCK = 0xAA;
+;   unlock to make a change (note PPSLOCKED is bit 0, the only active bit in PPSLOCK byte
+	clrf PPSLOCK  ;    PPSLOCKbits.PPSLOCKED = 0x00; // unlock PPS
+
+;   set up I2C on SSP1	
+;	movlb d'29'
+	movlw 0x13
+	movwf SSP1DATPPS ;    SSP1DATPPSbits.SSP1DATPPS = 0x13;   //RC3->MSSP1:SDA1;
+	movlw 0x14
+	movwf SSP1CLKPPS ;    SSP1CLKPPSbits.SSP1CLKPPS = 0x14;   //RC4->MSSP1:SCL1;
+	
+	movlb d'30'
+	movlw 0x15
+	movwf RC3PPS ;    RC3PPS = 0x15;   //RC3->MSSP1:SDA1;
+	movlw 0x14
+	movwf RC4PPS ;    RC4PPS = 0x14;   //RC4->MSSP1:SCL1
+
+;   set up SPI on SSP2	
+	movlb d'29'
+	movlw 0x12
+	movwf SSP2DATPPS ;    SSP2DATPPSbits.SSP1DATPPS = 0x12;   //RB7->MSSP2:SPIDAT;
+	movlw 0x0E
+	movwf SSP2CLKPPS ;    SSP2CLKPPSbits.SSP1CLKPPS = 0x0E;   //RB6->MSSP2:SPICL1;
+	movlw 0x0E
+	movwf SSP2SSPPS  ;
+	
+	movlb d'30'
+	movlw 0x17
+	movwf RB7PPS ;    RB7PPS = 0x17;   //RB7->MSSP2:SPIDAT;
+	movlw 0x16
+	movwf RB6PPS ;    RB6PPS = 0x16;   //RB6->MSSP2:SPICL1;
+	
+;   PPSLOCK takes a special sequence to lock
+	movlb d'29'
+	movlw 0x55
+	movwf PPSLOCK ;    PPSLOCK = 0x55;
+	movlw 0xAA
+	movwf PPSLOCK ;    PPSLOCK = 0xAA;
+	
+	movlw 0x01
+	movwf PPSLOCK ;    PPSLOCKbits.PPSLOCKED = 0x01; // lock PPS
+;
+;    GIE = state;
+	movf  INTCON,w	    ; get the current value
+	iorwf GIE_STATE	    ; OR with isolated bit 7
+	movwf INTCON	    ; store it
+;}  
+;   set up SPI on SSP2
+	call Init_SPI2	; SPI for DAC
+	
+	movlb 0		; reset to bank 0
+	return
+	
+; convert working C code from DualEG (mcc generated) to ASM
+Init_SPI2
+;    // Set the SPI2 module to the options selected in the User Interface
+	movlb 3
+;    // SMP Middle; CKE Idle to Active; = 0x00 MODE 1 when CKP Idle:Low, Active:High (not supported by MCP4922)
+;    // SMP Middle; CKE Active to Idle; = 0x40 MODE 0 when CKP Idle:Low, Active:High ( IS supported by MCP4922)
+;       0x20 is same as 0x40, but change clock to FOSC4 (8000kHz).  This was apparently too fast!!!	
+	movlw 0x40
+	movwf SSP2STAT   ;SSP2STAT = 0x00;
+;    
+;    // SSPEN enabled; CKP Idle:Low, Active:High; SSPM FOSC/4_SSPxADD;
+	movlw 0x2A
+	movwf SSP2CON1	;SSP2CON1 = 0x2A;
+;   
+;    // SSPADD 24; 
+	movlw 0x02     ;chg to 2 ( 1 did not work)
+	movwf SSP2ADD	;SSP2ADD = 0x02;
+	return
+	
+; convert working C code from DualEG (mcc generated) to ASM
+;void OSCILLATOR_Initialize(void)
+;{
+Init_Osc
+	movlb d'17'
+    ;// NOSC HFINTOSC; NDIV 1; 
+    ;OSCCON1 = 0x60;
+	movlw 0x60
+	movwf OSCCON1
+    ;// CSWHOLD may proceed; SOSCPWR Low power; 
+    ;OSCCON3 = 0x00;
+    	movlw 0x00		; yes, I know I can 'clrf' this, but it may change later
+	movwf OSCCON3
+    ;// MFOEN disabled; LFOEN disabled; ADOEN disabled; SOSCEN disabled; EXTOEN disabled; HFOEN disabled; 
+    ;OSCEN = 0x00;
+     	movlw 0x00
+	movwf OSCEN
+    ;// HFFRQ 32_MHz; 
+    ;OSCFRQ = 0x06;
+     	movlw 0x06
+	movwf OSCFRQ
+    ;// HFTUN 0; 
+    ;OSCTUNE = 0x00;
+     	movlw 0x00
+	movwf OSCTUNE
+    return
+;}
+
+    END
